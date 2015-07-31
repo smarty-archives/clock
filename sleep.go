e43b801e31ffc90c886e174061fa56089d20b12f
Whatcott, Michael <mdwhatcott@gmail.com>
2015-07-31 13:35:52 -0600

    RestoreSleep is now sleeper.Restore.

--- sleep.go
+++ sleep.go
@@ -8,17 +8,9 @@ import (
 // Sleep forwards to time.Sleep
 var Sleep = time.Sleep
 
-type Sleeper struct {
-	Naps []time.Duration
-	lock *sync.Mutex
-}
-
-func (this *Sleeper) Sleep(nap time.Duration) {
-	defer this.lock.Unlock()
-	this.lock.Lock()
-	this.Naps = append(this.Naps, nap)
-}
-
+// FakeSleep returns a *Sleeper instance and replace Sleep with *Sleeper.Sleep.
+// It is intended to be called from test code in order to mock calls to Now
+// in production code.
 func FakeSleep() *Sleeper {
 	sleeper := &Sleeper{
 		Naps: []time.Duration{},
@@ -28,6 +20,23 @@ func FakeSleep() *Sleeper {
 	return sleeper
 }
 
-func RestoreSleep() {
+// Sleeper stores calls to Sleep in its Naps slice.
+type Sleeper struct {
+	Naps []time.Duration
+	lock *sync.Mutex
+}
+
+// Substitute function for the package-level Sleep. Will be substituted as a
+// result of calling FakeSleep().
+func (this *Sleeper) Sleep(nap time.Duration) {
+	defer this.lock.Unlock()
+	this.lock.Lock()
+	this.Naps = append(this.Naps, nap)
+}
+
+// Restore assigns time.Sleep as the value for Sleep.
+// It is intended to be called from test code as cleanup after the actions under test
+// have been invoked.
+func (this *Sleeper) Restore() {
 	Sleep = time.Sleep
 }
